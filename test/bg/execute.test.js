50fb37398415972e66a9da01687a6995ed0217ce
Soldy <4786022+Soldy@users.noreply.github.com>
2025-07-25 14:08:58 +0100

    async test
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test/bg/execute.test.js
+++ test/bg/execute.test.js
@@ -1,12 +1,13 @@
 'use strict';
-describe('bg/execute', () => {
+describe('bg/execute', function(){
+  this.timeout(5000); 
   before(() => sinon.stub(UserScriptRegistry, 'scriptsToRunAt'));
   after(() => UserScriptRegistry.scriptsToRunAt.restore());
 
-  it('uses tabs.executeScript', () => {
+  it('uses tabs.executeScript', async () => {
     chrome.tabs.executeScript.callsArg(2);
     UserScriptRegistry.scriptsToRunAt.returns([{}]);
-    executeUserscriptOnNavigation({});
+    await executeUserscriptOnNavigation({});
     assert(chrome.tabs.executeScript.calledOnce);
   });
 });
